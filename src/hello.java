1daf427f0e336cb3fd53f819f30ab28e92384441
Hard_cheng <2463422967@qq.com>
2022-09-17 23:03:29 +0800

    新建

---
+++ src/hello.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName: hello
+ * @Description: TODO
+ * @Author: Hard_cheng
+ * @Date: 2022/9/17 23:02
+ * @Version: 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hcc");
+    }
+}
